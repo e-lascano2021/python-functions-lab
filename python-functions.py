e1ebe821bb98f227830f57166f52468d1ba4fde2
e.lascano <emilas5678@gmail.com>
2021-11-15 22:59:41 -0500

    finish challenge 1

---
+++ python-functions.py
@@ -0,0 +1,24 @@
+# Challenge 1
+
+def sum_to(n):
+  count = 0
+  for n in range(1, n + 1):
+    count += n
+  print(count)
+
+sum_to(6)
+sum_to(10)
+
+
+
+
+
+
+
+
+
+
+# Challenge 2
+def largest(list):
+  for num in list:
+    
